416256b4edb4c9b6ea3302e5c728e6cca76a1bf7
sunwook <tjsdnr9@gmail.com>
2025-08-05 11:38:48 +0900

    second commit

--- src/main/java/com/example/agent_server/AppController.java
+++ src/main/java/com/example/agent_server/AppController.java
@@ -19,7 +19,7 @@ public class AppController {
 
     @GetMapping("/home")
     public String home() {
-        return "home Action";
+        return "agent Action!!";
     }
 
     @GetMapping("/boards")
